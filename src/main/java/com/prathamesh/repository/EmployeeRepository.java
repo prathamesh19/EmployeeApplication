4fd05589330723f39e523c118fb6d3946a2315db
prathamesh19 <pratham.somnathe979@gmail.com>
2021-08-11 07:29:44 +0530

    done

--- src/main/java/com/prathamesh/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prathamesh.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prathamesh.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, String>{
-
-}
