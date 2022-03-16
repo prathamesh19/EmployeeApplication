e660aa5ba96eb26ac6ba192d84ad2eb6b26edb88
prathamesh19 <pratham.somnathe979@gmail.com>
2022-03-16 11:54:34 +0530

    modified

--- src/main/java/com/prathamesh/controller/HelloWorld.java
+++ src/main/java/com/prathamesh/controller/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.prathamesh.controller;
 
 public class HelloWorld {
-
+                 //HelloWorld App
 	public void message() {
 		System.out.println("HELLO WORLD....Prathamesh");
 	}
